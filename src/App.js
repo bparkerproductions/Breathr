bc12042972dac6ca274b1fa26ce3e4ae5899f95f
Brandon <bparkerproductions@gmail.com>
2019-06-18 00:10:13 -0800

    first card compoent and global styles

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
-let variable = 'hell';
-
-function comp() {
-  return <div>Hello</div>;
-}
 function App() {
   return (
     <div>
-    {variable}
-
-    <button className="button">
-      Button size normal
-    </button>
+    <section className="card intro-card column-center">
+      <div className="inner-container">
+        <div className="content">
+        <div className="header-container bottom-line">
+          <h2 className="title">Welcome to Breathr!</h2>
+        </div>
+        <p>This is a simple web app that lets you choose and collect your favorite sounds, and meditate to them. No special subscriptions, no extras. </p>
+        <p className="bold">Want to get started?</p>
+        <div className="button-container mt-small">
+        <button className="button">Get To It</button>
+        </div>
+        </div>
+      </div>
+    </section>
     </div>
   );
 }
