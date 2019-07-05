fc267144259362de4304a0bfd102397b469e0543
brandon parker <bparkerproductions@gmail.com>
2019-07-04 21:10:49 -0800

    pause and play functions

--- src/components/app/Controls/VideoControls.js
+++ src/components/app/Controls/VideoControls.js
@@ -9,43 +9,55 @@ class AppToggles extends React.Component {
 
     this.state = {
       muted: false,
-      volume: 100
+      volume: 100,
+      paused: false
     }
 
     this.toggleMuted = this.toggleMuted.bind(this);
+    this.togglePause = this.togglePause.bind(this);
     this.handleVolumeChange = this.handleVolumeChange.bind(this);
   }
 
+  //VOLUME related
   toggleMuted() {
     this.setState({ muted: !this.state.muted });
 
     let currentVolume = this.state.muted ? this.state.volume : 0;
+    console.log(currentVolume);
     this.props.videoPlayer.setVolume(currentVolume);
   }
-
   getMuteClass() {
     let isMuted = this.state.muted || this.state.volume === 0;
     return isMuted ? 'fas fa-volume-mute' : 'fas fa-volume-up';
   }
-
   handleVolumeChange(value) {
-    console.log('on change')
     this.setState({ volume: value });
     this.props.videoPlayer.setVolume(this.state.volume);
     return this.state.volume;
   }
-
   sliderDisabled() {
     return this.state.muted ? 'disabled' : '';
   }
-
   volumeDisabled() {
     return this.state.volume === 0 ? 'disabled' : '';
+  }
+
+  //PAUSE related
+  getPauseOrPlay() {
+    return this.state.paused ? 'fas fa-play' : 'far fa-pause-circle';
+  }
+  togglePause() {
+    this.setState({ paused: !this.state.paused });
+    let player = this.props.videoPlayer;
+    !this.state.paused ? player.pauseVideo() : player.playVideo();
   }
 
   render() {
     return (
       <aside className="navbar-col video-controls">
+        <div className="ui-button" onClick={this.togglePause}>
+          <i className={`far fa-pause-circle ${this.getPauseOrPlay()}`}></i>
+        </div>
         <div className={`ui-button ${this.volumeDisabled()}`} onClick={this.toggleMuted}>
           <i className={this.getMuteClass()}></i>
         </div>
@@ -54,7 +66,6 @@ class AppToggles extends React.Component {
           <Slider
             value={this.state.volume}
             onChange={this.handleVolumeChange}
-            onChangeComplete={this.handleVolumeChange}
             tooltip={false}>
           </Slider>
         </div>
