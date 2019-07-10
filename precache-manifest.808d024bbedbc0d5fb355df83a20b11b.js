905f3f44f6bbcbd1b9c4c864f2734ef2693dd6fc
brandon parker <bparkerproductions@gmail.com>
2019-07-09 20:51:31 -0800

    Updates

---
+++ precache-manifest.808d024bbedbc0d5fb355df83a20b11b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba409dff0327b3f3976e84533a71f1db",
+    "url": "/Breathr/index.html"
+  },
+  {
+    "revision": "1bce0b6a8e1d73651930",
+    "url": "/Breathr/static/css/2.fedfc1c4.chunk.css"
+  },
+  {
+    "revision": "4b20aa4883fc00c1d960",
+    "url": "/Breathr/static/css/main.5e45ed64.chunk.css"
+  },
+  {
+    "revision": "1bce0b6a8e1d73651930",
+    "url": "/Breathr/static/js/2.bcbc058f.chunk.js"
+  },
+  {
+    "revision": "4b20aa4883fc00c1d960",
+    "url": "/Breathr/static/js/main.ffcd9b81.chunk.js"
+  },
+  {
+    "revision": "a7047f086f2c25491ff5",
+    "url": "/Breathr/static/js/runtime~main.bd68d097.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/Breathr/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/Breathr/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/Breathr/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/Breathr/static/media/notification.c0d3c94c.eot"
+  }
+]);
