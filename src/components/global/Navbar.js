781b31fd23639cce38f102ebb10131612e104089
brandon parker <bparkerproductions@gmail.com>
2019-08-11 09:55:40 -0800

    mobile toggle implementation

--- src/components/global/Navbar.js
+++ src/components/global/Navbar.js
@@ -1,45 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AppToggles from './../app/Controls/AppToggles';
 import VideoControls from './../app/Controls/VideoControls';
 import DailyMinutes from './../app/Timer/DailyMinutes';
 import ToggleAll from './../app/Controls/ToggleAll';
 
-class Navbar extends React.Component {
-  render() {
-    return (
-      <nav className="column-center main-nav">
-        <div className="inner-container">
-          <div className="left">
-            <div className="mobile-content">
-              <div className="navbar-col">
-                <a className="white link"
-                  target="_blank"
-                  href="https://github.com/bparkerproductions/Breathr">
-                  Contribute
-                </a>
-              </div>
-              <div className="navbar-col">
-                <DailyMinutes></DailyMinutes>
-              </div>
-            </div>
-          </div>
+const Navbar = (props) => {
+  const [contentToggled, setContent] = useState(false);
 
-          <div className="right">
-            <div className="mobile-toggle">
-              <i class="fas fa-bars"></i>
+  function toggleContent() {
+    setContent(!contentToggled);
+  }
+  function isToggled() {
+    return contentToggled ? 'show' : '';
+  }
+  function getToggledState() {
+    return contentToggled ? 'fas fa-times-circle' : 'fas fa-bars';
+  }
+  return (
+    <nav className="column-center main-nav">
+      <div className="inner-container">
+        <div className={`left ${isToggled()}`}>
+          <div className="mobile-content">
+            <div className="navbar-col">
+              <a className="white link"
+                target="_blank"
+                href="https://github.com/bparkerproductions/Breathr">
+                Contribute
+              </a>
             </div>
-            <div class="video-content">
-              <VideoControls />
-              <div className="app-toggle-group">
-                <AppToggles />
-                <ToggleAll />
-              </div>
+            <div className="navbar-col">
+              <DailyMinutes></DailyMinutes>
             </div>
           </div>
         </div>
-      </nav>
-    );
-  }
+
+        <div className="right">
+          <div onClick={toggleContent} className="mobile-toggle">
+            <i className={getToggledState()}></i>
+          </div>
+          <div className="video-content">
+            <VideoControls />
+            <div className="app-toggle-group">
+              <AppToggles />
+              <ToggleAll />
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
