084e17ba5c90c518110382dcc65d5d2a47f4533e
Brandon Parker <bparkerproductions@gmail.com>
2022-01-15 11:01:11 -0800

    turn timerplayback into a functional component

--- src/components/app/Timer/TimerPlayback.js
+++ src/components/app/Timer/TimerPlayback.js
@@ -1,50 +1,42 @@
-import React from 'react';
+import React from 'react'
 
-class TimerPlayback extends React.Component {
-  getPlayOrPause(pause, resume) {
-    let pauseTimer = this.props.togglePauseCallback;
-
-    if(!this.props.paused) {
+const TimerPlayback = (props) => {
+  function getPlayOrPause(pause, resume) {
+    if(!props.paused) {
       return (
-        <div onClick={pauseTimer} className="pause">
+        <div onClick={props.togglePauseCallback} className="pause">
           <i onClick={pause} className="far fa-pause-circle"></i>
         </div>
-      );
+      )
     }
     else {
       return (
-        <div onClick={pauseTimer} className="resume">
+        <div onClick={props.togglePauseCallback} className="resume">
           <i onClick={resume} className="far fa-play-circle"></i>
         </div>
-      );
+      )
     }
   }
-  timerControls(pause, resume, reset) {
-    if(this.props.started) {
+
+  function timerControls(pause, resume, reset) {
+    if(props.started) {
       return (
         <div className="timer-controls">
-          {this.getPlayOrPause(pause, resume)}
+          {getPlayOrPause(pause, resume)}
           <div className="reset">
             <i onClick={reset} className="fas fa-undo"></i>
           </div>
         </div>
-      );
+      )
     }
-    else {
-      return null;
-    }
+    else return null
   }
-  render() {
-    let pause = this.props.pauseCallback;
-    let resume = this.props.resumeCallback;
-    let reset = this.props.resetCallback;
 
-    return (
-      <div className="playback">
-        {this.timerControls(pause, resume, reset)}
-      </div>
-    )
-  }
+  return (
+    <div className="playback">
+      {timerControls(props.pauseCallback, props.resumeCallback, props.resetCallback)}
+    </div>
+  )
 }
 
-export default TimerPlayback;
+export default TimerPlayback
