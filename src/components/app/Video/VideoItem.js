6ee8a7b2af6a5c77e5552c4b8db336948b001286
Brandon Parker <bparkerproductions@gmail.com>
2022-01-15 11:09:38 -0800

    turn videoItem into a functional component

--- src/components/app/Video/VideoItem.js
+++ src/components/app/Video/VideoItem.js
@@ -1,110 +1,97 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { selectVideo, addToCollection, removeFromCollection } from '../../../actions';
-import { NotificationManager } from 'react-notifications';
+import React from 'react'
+import { connect } from 'react-redux'
+import { selectVideo, addToCollection, removeFromCollection } from '../../../actions'
+import { NotificationManager } from 'react-notifications'
 
-class VideoItem extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.selectVideo = this.selectVideo.bind(this);
-    this.handleAdd = this.handleAdd.bind(this);
-    this.handleRemove = this.handleRemove.bind(this);
-  }
-
-  bgImage() {
-    let imageURL = this.props.video.snippet.thumbnails.medium.url;
+const VideoItem = (props) => {
+  function bgImage() {
     return {
-      backgroundImage: 'url(' + imageURL + ')'
+      backgroundImage: 'url(' + props.video.snippet.thumbnails.medium.url + ')'
     }
   }
 
-  selectVideo() {
-    let videoID = this.props.video.id.videoId;
-    this.props.selectVideo(videoID);
+  function selectVideo() {
+    props.selectVideo(props.video.id.videoId)
   }
 
-  doesVideoExist() {
-    let currentVideos = this.props.videos;
-    for(let i=0; i<currentVideos.length; i++) {
-      let videoID = currentVideos[i].id.videoId;
-      let selectedVideoID = this.props.video.id.videoId;
+  function doesVideoExist() {
+    for(let i=0; i<props.videos.length; i++) {
+      let videoID = props.videos[i].id.videoId
+      let selectedVideoID = props.video.id.videoId
 
-      if(videoID === selectedVideoID) return true; //already exists
+      if (videoID === selectedVideoID) return true //already exists
     }
 
-    return false; //doesn't exist yet
+    return false //doesn't exist yet
   }
 
-  handleAdd(e) {
-    e.stopPropagation();
+  function handleAdd(e) {
+    e.stopPropagation()
 
-    if(!this.doesVideoExist()) {
-      this.props.addToCollection(this.props.video);
+    if(!doesVideoExist()) {
+      props.addToCollection(props.video)
 
-      let successMessage = 'Your video has successfully been added to your collection!';
+      let successMessage = 'Your video has successfully been added to your collection!'
       NotificationManager.success(successMessage, 'Video Added!', 2000)
     }
     else {
-      let warningMessage = 'This video is already in your collection!';
+      let warningMessage = 'This video is already in your collection!'
       NotificationManager.warning(warningMessage, false, 2000)
     }
   }
 
-  handleRemove(e) {
-    e.stopPropagation();
-    let videoID = this.props.video.id.videoId;
-    this.props.removeFromCollection(videoID);
+  function handleRemove(e) {
+    e.stopPropagation()
+    let videoID = props.video.id.videoId
+    props.removeFromCollection(videoID)
 
-    let successMessage = 'Your video has successfully been removed from your collection!';
+    let successMessage = 'Your video has successfully been removed from your collection!'
     NotificationManager.success(successMessage, 'Video Removed!', 2000)
   }
 
-  renderControls() {
-    if(this.props.canAdd) {
+  function renderControls() {
+    if(props.canAdd) {
       return (
-        <div className="ui-button" onClick={this.handleAdd}>
+        <div className="ui-button" onClick={handleAdd}>
           <i title="add to collection" className="fas fa-plus-circle"></i>
         </div>
       )
     }
     else {
       return (
-        <div className="ui-button" onClick={this.handleRemove}>
+        <div className="ui-button" onClick={handleRemove}>
           <i title="remove from collection" className="fas fa-minus-circle red"></i>
         </div>
       )
     }
   }
 
-  render() {
-    return (
-      <div onClick={this.selectVideo} style={this.bgImage()} className="video-preview">
-        <div className="video-controls">
-          {this.renderControls()}
-        </div>
-        <div className="overlay-preview">
-          <p className="white title">
-            {this.props.video.snippet.title}
-          </p>
-          <div className="icon-container">
-            <i className="fas fa-play"></i>
-          </div>
+  return (
+    <div onClick={selectVideo} style={bgImage()} className="video-preview">
+      <div className="video-controls">
+        {renderControls()}
+      </div>
+      <div className="overlay-preview">
+        <p className="white title">
+          {props.video.snippet.title}
+        </p>
+        <div className="icon-container">
+          <i className="fas fa-play"></i>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 const mapStateToProps = state => {
   return {
     selectedVideo: state.selectedVideo,
     videos: state.videos
-  };
+  }
 }
 
 export default connect(mapStateToProps, {
   selectVideo,
   addToCollection,
   removeFromCollection
-})(VideoItem);
+})(VideoItem)
