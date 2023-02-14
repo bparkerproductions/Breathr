bfce4f9e3375643fec619f68daf5c294e26d6c29
Brandon Parker <bparkerproductions@gmail.com>
2023-02-14 09:15:41 -0800

    fix search bar calling excessive search queries

--- src/components/app/Search/SearchBar.js
+++ src/components/app/Search/SearchBar.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const SearchBar = (props) => {
-  function trackInput(event) {
-    props.searchCallback(event.target.value)
+  const [searchQuery, setSearchQuery] = useState('')
+
+  function trackInput() {
+    props.searchCallback(searchQuery)
   }
+
   function handleSubmit(e) {
     e.preventDefault()
   }
 
+  function updateSearchQuery(event) {
+    setSearchQuery(event.target.value)
+  }
+
+  function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      trackInput()
+    }
+  }
+
   return (
     <div className="search-input-container">
-      <div className="search-icon">
+      <div className="search-icon" onClick={trackInput}>
         <i className="fas fa-search"></i>
       </div>
       <div className="search-input">
         <form onSubmit={handleSubmit}>
-          <input type="text" onChange={trackInput}></input>
+          <input type="text" onKeyDown={handleKeyDown} onChange={updateSearchQuery}></input>
         </form>
       </div>
     </div>
--- src/helpers/apis/youtube.js
+++ src/helpers/apis/youtube.js
@@ -19,7 +19,7 @@ export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
-    maxResults: 10,
+    maxResults: 9,
     videoEmbeddable: true,
     type: 'video',
     videoDefinition: 'high',
