f6fd77a9a29cbd44e1742c42c94c0f0597d2a613
Brandon Parker <bparkerproductions@gmail.com>
2023-02-15 12:32:14 -0800

    helper function formatting and comments

--- src/helpers/store/general.js
+++ src/helpers/store/general.js
@@ -3,22 +3,31 @@ export function checkVisitCount() {
 }
 
 export function incrementVisitCount() {
-  //first visit, set to one
-  if(getData('visitAmounts') === null) localStorage.setItem('visitAmounts', 1);
+  // Set first visit, set to one
+  if(getData('visitAmounts') === null) localStorage.setItem('visitAmounts', 1)
 
-  //they visited again, increment the value
-  else localStorage.setItem('visitAmounts', getData('visitAmounts') + 1);
+  // They visited again, increment the value
+  else localStorage.setItem('visitAmounts', getData('visitAmounts') + 1)
 }
 
+/**
+ * Store a generic object in local storage
+ */
 export function store(newArray, storageObj) {
-  let stringified = JSON.stringify(newArray);
-  localStorage.setItem(storageObj, JSON.stringify(stringified));
+  let stringified = JSON.stringify(newArray)
+  localStorage.setItem(storageObj, JSON.stringify(stringified))
 }
 
+/**
+ * Get a generic object from local storage
+ */
 export function getData(obj) {
-  return JSON.parse(localStorage.getItem(obj));
+  return JSON.parse(localStorage.getItem(obj))
 }
 
+/**
+ * Create the initial object in local storage for tracking session minutes
+ */
 export function createTimeTrack() {
-  if(getData('timeTrack') === null) store({}, 'timeTrack');
+  if (getData('timeTrack') === null) store({}, 'timeTrack')
 }
--- src/helpers/store/timeStore.js
+++ src/helpers/store/timeStore.js
@@ -1,33 +1,36 @@
-import { store, getData } from './general';
-import { createTimeTrack } from './general';
+import { store, getData } from './general'
+import { createTimeTrack } from './general'
 
 export function storeSeconds(seconds) {
-  store(seconds, 'totalSeconds');
-  storeTime();
+  store(seconds, 'totalSeconds')
+  storeTime()
 }
 
 export function getTotalSeconds() {
-  return getData('totalSeconds') === '[]' ? 0 : getData('totalSeconds');
+  return getData('totalSeconds') === '[]' ? 0 : getData('totalSeconds')
 }
 
 export function getFormattedDate(date) {
-  return date.toISOString().slice(0, 10);
+  return date.toISOString().slice(0, 10)
 }
 
+/**
+ * Get current date, then create new entry in localstorage
+ * with the current MM-DD-YYY timestamp + total seconds for the day
+ */
 export function storeTime() {
-  //get current date, then create new entry in localstorage
-  //with the current MM-DD-YYY timestamp + total seconds for the day
-  let timeTrack = getData('timeTrack') === '{}' ? JSON.parse(getData('timeTrack')) : getData('timeTrack');
+  let timeTrack = getData('timeTrack') === '{}' ? JSON.parse(getData('timeTrack')) : getData('timeTrack')
 
-  if(timeTrack[getFormattedDate(new Date())] === undefined) {
-    timeTrack[getFormattedDate(new Date())] = 0;
+  if (timeTrack[getFormattedDate(new Date())] === undefined) {
+    timeTrack[getFormattedDate(new Date())] = 0
   }
-  timeTrack[getFormattedDate(new Date())]++;
-  localStorage.setItem('timeTrack', JSON.stringify(timeTrack));
+
+  timeTrack[getFormattedDate(new Date())]++
+  localStorage.setItem('timeTrack', JSON.stringify(timeTrack))
 }
 
 export function getTimeForDay() {
-  createTimeTrack();
+  createTimeTrack()
 
-  return getData('timeTrack')[getFormattedDate(new Date())];
+  return getData('timeTrack')[getFormattedDate(new Date())]
 }
