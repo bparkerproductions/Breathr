c342c51cc8713e8f46458bd1f0e135d40d13d2c0
Brandon Parker <bparkerproductions@gmail.com>
2023-02-24 13:52:23 -0800

    intro modal open collection

--- src/components/global/IntroModal.js
+++ src/components/global/IntroModal.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux'
 import { checkVisitCount } from '../../helpers/store/general'
 import { setPaused } from './../../actions/appToggles'
 import { incrementVideosPlayed } from './../../actions'
+import { toggleCollection } from './../../actions/appToggles'
+
 
 const IntroModal = (props) => {
   function playVideo() {
@@ -17,12 +19,18 @@ const IntroModal = (props) => {
     props.setPaused(false)
   }
 
+  function openCollection() {
+
+    if (!props.isCollection)
+      props.toggleCollection()
+  }
+
   function title() {
     return checkVisitCount() > 1 ? 'Welcome Back!' : 'Welcome to Breathr!'
   }
 
   function description() {
-    const welcomeMessage = 'Click the button to continue enjoying your personalized sounds..'
+    const welcomeMessage = 'Pick up where you left off'
     const firstTimeMessage = 'Breathr lets you choose and collect your favorite soundscapes/moods for meditation and relaxation. No special subscriptions, no extras.'
     return checkVisitCount() > 1 ? welcomeMessage : firstTimeMessage
   }
@@ -42,18 +50,17 @@ const IntroModal = (props) => {
               <hr />
             </div>
 
-            <div className="row">
-              <div className="col-12 col-lg-6">
-                <p>{description()}</p>
-                <div className="play-container d-flex">
-                  <button onClick={playVideo} className="btn btn-primary py-3 px-4 text-dark">
-                    <span className="fw-bold text-white">Start Video Now</span>
-                    <i className="fas fa-play ms-2 fa-lg text-dark ms-3"></i>
-                  </button>
-                </div>
-              </div>
-              <div className="col-12 col-lg-6 mt-3 border-start">
-                Open Collection
+            <div>
+              <p>{description()}</p>
+              <div className="play-container d-flex flex-column flex-md-row">
+                <button onClick={playVideo} className="btn btn-primary py-3 px-4 text-dark mb-2 mb-md-0 border">
+                  <span className="fw-bold text-white">Start Video Now</span>
+                  <i className="fas fa-play ms-2 fa-lg text-white ms-3"></i>
+                </button>
+                <button onClick={openCollection} className="btn btn-secondary fw-bold text-white py-3 px-4 ms-0 ms-md-2 border">
+                  Open Collection
+                  <i className="fas fa-bookmark ms-2 fa-lg text-white ms-3"></i>
+                </button>
               </div>
             </div>
         </Modal>
@@ -65,10 +72,12 @@ const IntroModal = (props) => {
 const mapStateToProps = state => {
   return {
     videosPlayed: state.videosPlayed,
+    isCollection: state.isCollectionToggled
   }
 }
 
 export default connect(mapStateToProps, {
   setPaused,
-  incrementVideosPlayed
+  incrementVideosPlayed,
+  toggleCollection
 })(IntroModal)
