0afb4331240e205a930621e4ee640050ad761dd1
Brandon Parker <bparkerproductions@gmail.com>
2023-02-24 23:14:23 -0800

    fix timer callbacks

--- src/components/app/Timer/TimerPlayback.js
+++ src/components/app/Timer/TimerPlayback.js
@@ -1,31 +1,41 @@
 import React from 'react'
 
 const TimerPlayback = (props) => {
-  function getPlayOrPause(pause, resume) {
+  function getPlayOrPause() {
     const buttonClasses = "btn btn-primary text-white";
     if (!props.paused) {
       return (
-        <div onClick={props.togglePauseCallback} className={`pause ${buttonClasses}`}>
-          <i onClick={pause} className="far fa-pause-circle fa-lg"></i>
+        <div onClick={pauseCallback} className={`pause ${buttonClasses}`}>
+          <i className="far fa-pause-circle fa-lg"></i>
         </div>
       )
     }
     else {
       return (
-        <div onClick={props.togglePauseCallback} className={`resume ${buttonClasses}`}>
-          <i onClick={resume} className="far fa-play-circle fa-lg"></i>
+        <div onClick={resumeCallback} className={`resume ${buttonClasses}`}>
+          <i className="far fa-play-circle fa-lg"></i>
         </div>
       )
     }
+  }
+
+  function resumeCallback() {
+    props.togglePauseCallback()
+    props.resumeCallback()
+  }
+
+  function pauseCallback() {
+    props.togglePauseCallback()
+    props.pauseCallback()
   }
 
   function timerControls() {
     if (props.started) {
       return (
         <div className="timer-controls d-flex justify-content-center">
-          {getPlayOrPause(props.pauseCallback, props.resumeCallback)}
-          <div className="reset btn btn-primary text-white ms-2">
-            <i onClick={resetTimer} className="fas fa-undo fa-lg"></i>
+          {getPlayOrPause()}
+          <div onClick={resetTimer} className="reset btn btn-primary text-white ms-2">
+            <i className="fas fa-undo fa-lg"></i>
           </div>
         </div>
       )
