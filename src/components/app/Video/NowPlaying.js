7a003a13d7b8ba9ba36291f5dc3b6e2a819b632a
Brandon Parker <bparkerproductions@gmail.com>
2023-10-30 16:58:02 -0700

    now playing component refactor to mui

--- src/components/app/Video/NowPlaying.js
+++ src/components/app/Video/NowPlaying.js
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
-import ToolTip from '../../elements/ToolTip'
+// import ToolTip from '../../elements/ToolTip'
 
-import Box from '@mui/joy/Box';
+import IconButton from '@mui/joy/IconButton'
+import Button from '@mui/joy/Button'
+import Box from '@mui/joy/Box'
+import Tooltip from '@mui/joy/Tooltip'
+import Card from '@mui/joy/Card'
+import Divider from '@mui/joy/Divider'
+import Typography from '@mui/joy/Typography'
+import Link from '@mui/joy/Link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+import { faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { getByDisplayValue } from '@testing-library/react'
 
 const NowPlaying = props => {
   const [tooltipToggled, toggleTooltip] = useState(false)
@@ -26,41 +35,60 @@ const NowPlaying = props => {
     toggleTooltip(!tooltipToggled)
   }
 
+  function getHidden() {
+    if (!tooltipToggled) return 'none'
+  }
+
   return (
-    <div id="nowPlaying" className="ms-3 ms-lg-0 me-2 pe-2 position-relative">
-      <Box onClick={tooltipActivated}>
-        <FontAwesomeIcon
-          icon={faInfoCircle}
-          className="ui-button"
-          size="lg"
-        />
-      </Box>
-      <ToolTip 
-        toggledOuter={tooltipToggled} 
-        closeTooltip={() => { toggleTooltip(false) }}
+    <Box id="now-playing" sx={{ position: "relative" }}>
+      <Tooltip title="Toggle video information" variant="solid">
+        <IconButton onClick={tooltipActivated}>
+          <FontAwesomeIcon
+            icon={faInfoCircle}
+            className="ui-button"
+            size="lg"
+          />
+        </IconButton>
+      </Tooltip>
+
+      <Card
+        orientation="vertical"
+        variant="soft"
+        id="now-playing-info"
+        sx={{
+          width: 350,
+          position: "absolute",
+          display: getHidden(),
+          marginTop: 1
+        }}
       >
-        <ul className="list-unstyled m-0 p-0">
-          <li className="pb-2 mb-2 border-bottom">
-            <a 
-              target="_blank"
-              rel="noreferrer"
-              title="Open this video in Youtube"
-              href={getPlayerInfo('videoUrl')}
-            >
-              <i className="fab fa-youtube text-danger fa-2x cursor-pointer"></i>
-            </a>
-          </li>
-          <li className="pt-1 mt-1 pb-2 mb-2 border-bottom">
-            <p className="fw-bold text-primary mb-1">Now Playing</p>
-            <p className="fw-light">{getVideoData('title')}</p>
-          </li>
-          <li className="py-1 my-1 border-bottom">
-            <p className="fw-bold text-primary mb-1">Duration</p>
-            <p className="fw-light">{Math.ceil(getPlayerInfo('duration')/60)} minutes</p>
-          </li>
-        </ul>
-      </ToolTip>
-    </div>
+
+        <Button
+          target="_blank"
+          component="a"
+          color="danger"
+          href={getPlayerInfo('videoUrl')}
+          startDecorator={<FontAwesomeIcon
+            icon={faYoutube}
+            className="ui-button"
+            size="lg"
+          />}
+        >
+          Open in youtube
+        </Button>
+        <Divider />
+        <Box>
+          <Typography level="body-md" fontWeight="bold" marginBottom="0">Now Playing</Typography>
+          <Typography level="body-md" fontWeight="light">{getVideoData('title')}</Typography>
+        </Box>
+        <Divider />
+
+        <Box>
+          <Typography fontWeight="bold">Duration</Typography>
+          <Typography fontWeight="light">{Math.ceil(getPlayerInfo('duration')/60)} minutes</Typography>
+        </Box>
+      </Card>
+    </Box>
   )
 }
 
