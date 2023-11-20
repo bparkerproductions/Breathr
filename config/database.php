24421f68ce4cba451557fbd3be5b9ba4b04715b2
Brandon Parker <bparkerproductions@gmail.com>
2023-11-20 12:58:59 -0800

    config db connection for local or prod

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Str;
 
-return [
+$connections = [
 
     /*
     |--------------------------------------------------------------------------
@@ -41,26 +41,6 @@ return [
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-        ],
-
-        'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('JAWSDB_URL'),
-            'host' => parse_url(env('JAWSDB_URL'), PHP_URL_HOST),
-            'port' => parse_url(env('JAWSDB_URL'), PHP_URL_PORT),
-            'database' => substr(parse_url(env('JAWSDB_URL'), PHP_URL_PATH), 1),
-            'username' => parse_url(env('JAWSDB_URL'), PHP_URL_USER),
-            'password' => parse_url(env('JAWSDB_URL'), PHP_URL_PASS),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
         ],
 
         'pgsql' => [
@@ -149,3 +129,47 @@ return [
     ],
 
 ];
+
+if (env('JAWSDB_URL')) {
+    $connections['connections']['mysql'] = [
+        'driver' => 'mysql',
+        'url' => env('JAWSDB_URL'),
+        'host' => parse_url(env('JAWSDB_URL'), PHP_URL_HOST),
+        'port' => parse_url(env('JAWSDB_URL'), PHP_URL_PORT),
+        'database' => substr(parse_url(env('JAWSDB_URL'), PHP_URL_PATH), 1),
+        'username' => parse_url(env('JAWSDB_URL'), PHP_URL_USER),
+        'password' => parse_url(env('JAWSDB_URL'), PHP_URL_PASS),
+        'unix_socket' => env('DB_SOCKET', ''),
+        'charset' => 'utf8mb4',
+        'collation' => 'utf8mb4_unicode_ci',
+        'prefix' => '',
+        'prefix_indexes' => true,
+        'strict' => true,
+        'engine' => null,
+        'options' => extension_loaded('pdo_mysql') ? array_filter([
+            PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+        ]) : [],
+    ];
+}
+else {
+    $connections['connections']['mysql'] = [
+        'driver' => 'mysql',
+        'url' => env('DATABASE_URL'),
+        'host' => env('DB_HOST', '127.0.0.1'),
+        'port' => env('DB_PORT', '3306'),
+        'database' => env('DB_DATABASE', 'forge'),
+        'username' => env('DB_USERNAME', 'forge'),
+        'password' => env('DB_PASSWORD', ''),
+        'unix_socket' => env('DB_SOCKET', ''),
+        'charset' => 'utf8mb4',
+        'collation' => 'utf8mb4_unicode_ci',
+        'prefix' => '',
+        'prefix_indexes' => true,
+        'strict' => true,
+        'engine' => null,
+        'options' => extension_loaded('pdo_mysql') ? array_filter([
+            PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+        ]) : [],
+    ];
+}
+return $connections;
